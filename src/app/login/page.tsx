f1e3e14afab23503e5c81406b74bbe857212f907
arisevan <115055847+evan9609@users.noreply.github.com>
2024-01-30 00:56:52 +0800

    login class

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,29 +1,38 @@
 "use client"
+import { TextTitle } from '@/components/Text';
+import { ButtonLogin } from '@/components/Buttons';
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Login(){
   return (
-    <div className="flex h-full min-h-custom justify-center items-center">
-      <div className="my-[60px] mx-[20px] flex flex-col bg-white p-[40px] rounded-md">
-        <div className="text-indigo-700">Don't have any account?</div>
-        <form action="">
-          <label>
-            <div>email:</div>
-            <input type="text" className="border-2"/>
-          </label>
-          <label>
-            <div>password:</div>
-            <input type="text" className="border-2"/>
-          </label>
-          <button className="block">Log in</button>
-        </form>
-        <div>
-          or
+    <div className="flex h-full min-h-custom justify-center items-center py-[60px] px-[20px]">
+      <div className="flex flex-col  max-w-80 w-full">
+        <div className='text-center'>
+          <TextTitle text="Welcome to <br>Pexels Collection" delay={1}></TextTitle>
         </div>
-        <div className="flex flex-col">
-          <button className="p-3 text-left" onClick={()=>{signIn('google')}}>Log In With Google</button>
-          <button className="p-3 text-left">Log In With Facebook</button>
-          <button className="p-3 text-left">Log In With Github</button>
+        <div className='bg-white rounded-lg py-[40px] px-[20px]'>
+          <div className="text-neutral-600 text-sm">Don't have any account?
+          </div>
+          <div className='text-teal-600 font-bold text-right transition-all  hover:text-orange-400 ml-auto text-sm cursor-pointer'>sign up for free</div>
+          <form action="" className="mt-4">
+            <label>
+              <div>email:</div>
+              <input type="text" className="border-2"/>
+            </label>
+            <label>
+              <div>password:</div>
+              <input type="text" className="border-2"/>
+            </label>
+            <ButtonLogin event={()=>{}} addClass="bg-amber-300 mt-3">Log In</ButtonLogin>
+          </form>
+          <div>
+            or
+          </div>
+          <div className="flex flex-col gap-3 mt-3">
+            <ButtonLogin event={()=>{signIn('google')}}>Log In With Google</ButtonLogin>
+            <ButtonLogin event={()=>{signIn('google')}}>Log In With Facebook</ButtonLogin>
+            <ButtonLogin event={()=>{signIn('google')}}>Log In With Github</ButtonLogin>
+          </div>
         </div>
       </div>
     </div>
