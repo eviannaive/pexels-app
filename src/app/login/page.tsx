4f4c696a0ba9e653d8ca6d888b497d523cf451e3
arisevan <115055847+evan9609@users.noreply.github.com>
2024-01-31 00:23:25 +0800

    login ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 import { TextTitle } from '@/components/Text';
 import { ButtonLogin } from '@/components/Buttons';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Login(){
   return (
@@ -10,28 +11,33 @@ export default function Login(){
         <div className='text-center'>
           <TextTitle text="Welcome to <br>Pexels Collection" delay={1}></TextTitle>
         </div>
-        <div className='bg-white rounded-lg py-[40px] px-[20px]'>
+        <div className='bg-white rounded-lg py-[35px] px-[20px]'>
           <div className="text-neutral-600 text-sm">Don't have any account?
           </div>
           <div className='text-teal-600 font-bold text-right transition-all  hover:text-orange-400 ml-auto text-sm cursor-pointer'>sign up for free</div>
-          <form action="" className="mt-4">
-            <label>
-              <div>email:</div>
-              <input type="text" className="border-2"/>
-            </label>
-            <label>
-              <div>password:</div>
-              <input type="text" className="border-2"/>
-            </label>
-            <ButtonLogin event={()=>{}} addClass="bg-amber-300 mt-3">Log In</ButtonLogin>
+          <form action="" className="mt-4 text-neutral-600">
+            <input type="text" className="border-2 w-full mt-1 rounded-md focus:outline-none p-1" placeholder="Email"/>
+            <input type="text" className="border-2 w-full rounded-md focus:outline-none p-1 mt-2" placeholder="Password"/>
+            <ButtonLogin event={()=>{}} addClass="bg-violet-600 mt-3 border-none text-white">Log In</ButtonLogin>
           </form>
-          <div>
-            or
+          <div className='flex items-center mt-3'>
+            <div className='w-full h-0.5 bg-slate-500/40'></div>
+            <span className='mx-2 text-slate-500'>OR</span>
+            <div className='w-full h-0.5 bg-slate-500/40'></div>
           </div>
           <div className="flex flex-col gap-3 mt-3">
-            <ButtonLogin event={()=>{signIn('google')}}>Log In With Google</ButtonLogin>
-            <ButtonLogin event={()=>{signIn('google')}}>Log In With Facebook</ButtonLogin>
-            <ButtonLogin event={()=>{signIn('google')}}>Log In With Github</ButtonLogin>
+            <ButtonLogin event={()=>{signIn('google')}}>
+              <Image src="https://authjs.dev/img/providers/google.svg" width="22" height="22" alt="google"/>
+              <p>Log In With Google</p>
+            </ButtonLogin>
+            <ButtonLogin event={()=>{signIn('google')}}>
+              <Image src="https://authjs.dev/img/providers/facebook.svg" width="24" height="24" alt="facebook"/>
+              <p>Log In With Facebook</p>
+            </ButtonLogin>
+            <ButtonLogin event={()=>{signIn('google')}} addClass='bg-stone-900'>
+              <Image src="https://authjs.dev/img/providers/github.svg" width="22" height="22" alt="github"/>
+              <p className='text-white'>Log In With Github</p>
+            </ButtonLogin>
           </div>
         </div>
       </div>
--- src/components/Buttons/ButtonLogin.tsx
+++ src/components/Buttons/ButtonLogin.tsx
@@ -2,6 +2,6 @@
 
 export default function ButtonLogin({event,addClass,children}:{event:any,addClass?:string,children: React.ReactNode}){
   return(
-    <button className={`p-3 text-left py-[10px] px-[20px] border border-slate-300 rounded-50px w-full ${addClass}`} onClick={event}>{children}</button>
+    <button className={`flex gap-2 items-center p-3 text-left py-[10px] px-[20px] border border-slate-300 rounded-50px w-full text-neutral-600 transition duration-300 hover:border-violet-400 ${addClass}`} onClick={event}>{children}</button>
   )
 }
