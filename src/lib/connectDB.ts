082e8600e0d2c4a3da9dad9f88f0622aaac9e451
evianna <xinyu721.k@gmail.com>
2024-03-03 17:01:01 +0800

    debug

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -1,18 +1,12 @@
 import mongoose from "mongoose";
 
-// let isConnected = false;
-console.log(process.env.MONGODB_CONNECTION,'urllll')
+const mongodbUrl = process.env.MONGODB_CONNECTION!!
 
 export async function connectDB(){
-  // if(isConnected){
-  //   console.log('DB connected already!')
-  //   return;
-  // }
   try{
-    console.log('連接DB...npm run dev');
-    await mongoose.connect(String(process.env.MONGODB_CONNECTION)).then(()=>{
+    console.log('connecting DB...');
+    await mongoose.connect(mongodbUrl).then(()=>{
       console.log('connect DB successful')
-      // isConnected = true
     })
   }catch(error){
     console.log(error)
